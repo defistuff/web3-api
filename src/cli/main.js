3104d1518a24b05f369be757493ebb280348a21c
Luis Acerv <luis@catalyst.com.mx>
2020-10-08 19:45:07 -0500

    remove useless file

--- src/cli/main.js
+++
@@ -1,9 +0,0 @@
-const { argv } = require('yargs');
-
-const Ethereum = require('../modules/ethereum');
-
-(async function () {
-    const method = argv.method;
-    const f = Ethereum[method];
-    console.log(await f());
-})();
